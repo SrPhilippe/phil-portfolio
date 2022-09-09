9e34c805f5e7e4b51403a97db31d6bb2012fd715
SrPhilippe <philippehenrique@outlook.com>
2022-09-09 01:04:25 -0300

    update

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,12 @@
-console.log(1+1)
+function toggleMenu(menuEl) {
+    console.log(menuEl)
+    // stop the default event of A element
+}
+
+const   $navbar = document.querySelector('nav.menu ul:first-child')
+toggleMenu($navbar.children)
+
+
+window.addEventListener("resize", (event) => {
+    console.log(event)
+})
