f43d4501bb36d4ffb7e77fe3daec7caa76d4baf6
SrPhilippe <philippehenrique@outlook.com>
2022-09-12 16:13:11 -0300

    sticky menu javascript

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,39 @@
-function toggleMenu(menuEl) {
-    console.log(menuEl)
-    // stop the default event of A element
+const $navbar = document.querySelector('nav.menu ul:first-child'),
+	$header = document.querySelector('header')
+
+let scrollvalue = window.scrollY,
+	devicewidth = window.innerWidth,
+	headerheight = $header.offsetHeight,
+	sticky = null,
+	ticking = false
+
+// To define the initial state of the scroll
+scrollvalue >= headerheight ? (sticky = true) : (sticky = false)
+window.addEventListener('load', menuSticky())
+
+console.log(sticky)
+
+function menuSticky() {
+	if (!ticking) {
+		window.requestAnimationFrame(() => {
+			if (scrollvalue >= headerheight && sticky === true) {
+				$header.classList.add('sticky')
+				document.body.style.paddingTop =
+					headerheight + $header.offsetHeight + 'px'
+				sticky = false
+			} else if (scrollvalue < headerheight && sticky === false) {
+				$header.classList.remove('sticky')
+				headerheight = $header.offsetHeight
+				document.body.style.paddingTop = '0'
+				sticky = true
+			}
+			ticking = false
+		})
+		ticking = true
+	}
 }
 
-const   $navbar = document.querySelector('nav.menu ul:first-child')
-toggleMenu($navbar.children)
-
-
-window.addEventListener("resize", (event) => {
-    console.log(event)
+window.addEventListener('scroll', (event) => {
+	scrollvalue = window.scrollY
+    menuSticky()
 })
