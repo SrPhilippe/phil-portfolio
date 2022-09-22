68ef4e532faae7285003f85b7bf13f42b1397a9a
SrPhilippe <philippehenrique@outlook.com>
2022-09-22 13:04:08 -0300

    format fixes and disposing

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,14 @@
 import { apiKeys, mailPrefs } from './config.js'
-import FormValidator from './formValidator.js'
+// import FormValidator from './formValidator.js'
 
-const $navbar = document.querySelector('nav.menu'),
-	$header = document.querySelector('header'),
-	$form = document.querySelector('.contact-form')
-
+const $navbar = document.querySelector('nav.menu')
+const $header = document.querySelector('header')
+const $form = document.querySelector('.contact-form')
 
 let scrollvalue = window.scrollY,
 	devicewidth = window.innerWidth,
 	headerheight = $header.offsetHeight,
-	sticky = null,
-	ticking = false
+	sticky = null
 
 if (devicewidth < 768) {
 	if (document.querySelector('.menu.mobile') === null) {
@@ -18,29 +16,52 @@ if (devicewidth < 768) {
 	}
 }
 
+window.addEventListener('load', ev => {
+	$header.style.top = `-${headerheight}px` // to have a slide down animation
+	scrollvalue >= headerheight ? (sticky = true) : (sticky = false)
+	menuSticky()
+	correctElDetails()
+})
+
+window.addEventListener('scroll', ev => {
+	scrollvalue = window.scrollY
+	menuSticky()
+
+	scrollvalue > headerheight ? show($scrollTop, null) : hide($scrollTop, null)
+})
+
+window.addEventListener('resize', ev => {
+	correctElDetails()
+	if (window.innerWidth < 768) {
+	}
+})
+
+// Select all inputs inside the contact form excluding the input submit type
+const inputsForm = $form.querySelectorAll('input:not([type="submit"], [type="hidden"]), textarea')
+inputsForm.forEach(el => {
+	el.addEventListener('input', ev => {
+		ev.currentTarget.value.length > 0
+			? (ev.currentTarget.classList.add('active'), ev.currentTarget.previousElementSibling.classList.add('active'))
+			: (ev.currentTarget.classList.remove('active'),
+			  ev.currentTarget.previousElementSibling.classList.remove('active'))
+	})
+})
+
 function menuSticky() {
-	if (!ticking) {
-		window.requestAnimationFrame(() => {
-			if (scrollvalue >= headerheight && sticky === true) {
-				$header.classList.add('sticky')
-				document.body.style.paddingTop =
-					headerheight + $header.offsetHeight + 'px'
-				sticky = false
-			} else if (scrollvalue < headerheight && sticky === false) {
-				$header.classList.remove('sticky')
-				headerheight = $header.offsetHeight
-				document.body.style.paddingTop = '0'
-				sticky = true
-			}
-			ticking = false
-		})
-		ticking = true
+	if (scrollvalue >= headerheight && sticky === true) {
+		$header.classList.add('sticky')
+		document.body.style.paddingTop = headerheight + $header.offsetHeight + 'px'
+		sticky = false
+	} else if (scrollvalue < headerheight && sticky === false) {
+		$header.classList.remove('sticky')
+		headerheight = $header.offsetHeight
+		document.body.style.paddingTop = '0'
+		sticky = true
 	}
 }
 
 function showNavigation() {
 	const $mobNav = document.createElement('div')
-
 	$mobNav.classList.add('menu', 'mobile')
 	for (let i = 0; i < 3; i++) {
 		let el = document.createElement('span')
@@ -55,63 +76,56 @@ function correctElDetails() {
 	$el.style.borderTopWidth = `${$el.parentNode.offsetHeight}px`
 }
 
-window.addEventListener('load', () => {
-	scrollvalue >= headerheight ? (sticky = true) : (sticky = false)
-	menuSticky()
-	correctElDetails()
-})
-
-window.addEventListener('scroll', (ev) => {
-	scrollvalue = window.scrollY
-	menuSticky()
-})
-
-window.addEventListener('resize', (ev) => {
-	if (window.innerWidth < 768) {
-	}
-
-	correctElDetails()
-})
-
-// Select all inputs inside the contact form excluding the input submit type
-$form.querySelectorAll('input:not([type="submit"], [type="hidden"]), textarea').forEach(el => {
-	el.addEventListener('input', ev => {
-		ev.currentTarget.value.length > 0
-			? (ev.currentTarget.classList.add('active'), ev.currentTarget.previousElementSibling.classList.add('active'))
-			: (ev.currentTarget.classList.remove('active'), ev.currentTarget.previousElementSibling.classList.remove('active'))
+const $scrollTop = document.querySelector('.scroll-top')
+$scrollTop.addEventListener('click', ev => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
 	})
 })
 
 emailjs.init(apiKeys.public.emailjs)
 
-$form.addEventListener('submit', (ev) => {
+$form.addEventListener('submit', ev => {
 	ev.preventDefault()
+
+	
+
 	const $loading = document.querySelector('.sc.contact .popup')
 	const $info = $loading.querySelector('.info')
 	const $ring = $loading.querySelector('.lds-ring')
 
-	
 	$ring.style.display = 'inline-block'
 	$info.textContent = `Enviando`
-	$loading.style.visibility = 'visible'
-	
-	// just a small delay to let the css transition happen
-	window.setTimeout(() => $loading.style.opacity = 1, 30) 
+	show($loading)
 
 	// Generate five digits number to be used as message ID
-	ev.target.id_number.value = Math.random() * 100000 | 0
-	ev.target.to_name.value = "Philippe"
-	emailjs.sendForm(mailPrefs.contactService, mailPrefs.templateId, ev.target)
-	.then(() => {
+	ev.target.id_number.value = (Math.random() * 100000) | 0
+	ev.target.to_name.value = 'Philippe'
+	emailjs.sendForm(mailPrefs.contactService, mailPrefs.templateId, ev.target).then(() => {
 		// clearForm()
 		$ring.style.display = 'none'
 		$info.textContent = `Sua mensagem foi enviada!`
 		window.setTimeout(() => {
-			$loading.style.opacity = 0
-			window.setTimeout(() => $loading.style.visibility = 'hidden', 300)
+			hide($loading)
 		}, 2000)
-	}), error => {
-		console.log('Failed...', error)
-	}
-
+	}),
+		error => {
+			$ring.style.display = 'none'
+			$info.textContent = `Houve um erro ao enviar a mensagem.`
+			console.log('Failed...', error)
+		}
 })
+
+function show(el) {
+	el.style.visibility = 'visible'
+	window.setTimeout(() => (el.style.opacity = 1), 30)
+}
+
+function hide(el, animationTime) {
+	if (animationTime === null) {
+		animationTime = 300
+	}
+	el.style.opacity = 0
+	window.setTimeout(() => (el.style.visibility = 'hidden'), animationTime)
+}
