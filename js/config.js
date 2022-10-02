15f718aec8df9cb1bc3bc623c238d851117203cc
SrPhilippe <philippehenrique@outlook.com>
2022-10-02 17:13:27 -0300

    removed git API key

--- js/config.js
+++ js/config.js
@@ -3,8 +3,7 @@ export { apiKeys, mailPrefs }
 
 const apiKeys = {
     public: {
-        emailjs: "9CEj6LfIzI9NzUiv_",
-        github: "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"
+        emailjs: "9CEj6LfIzI9NzUiv_"
     }
 }
 
