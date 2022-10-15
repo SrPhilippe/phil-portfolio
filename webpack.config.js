3442e8e097454070714f2e840a21ed5f7f1abf67
SrPhilippe <philippehenrique@outlook.com>
2022-10-15 19:35:00 -0300

    waching html, clean in production

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const stylesHandler = MiniCssExtractPlugin.loader
 
 const isProduction = process.env.NODE_ENV == 'production'
+const stylesHandler = MiniCssExtractPlugin.loader
 
 const config = {
     entry: {
@@ -15,12 +15,12 @@ const config = {
         ]
     },
     output: {
-        path: path.resolve(__dirname, 'docs'),
-        clean: true
+        path: path.resolve(__dirname, 'docs')
     },
     devServer: {
-        open: true,
-        host: 'localhost'
+        open: false,
+        host: 'localhost',
+        watchFiles: ['./index.html']
     },
     optimization: {
         minimizer: [
@@ -51,19 +51,20 @@ const config = {
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                type: 'asset/resource',
             },
             {
                 test: /\.html$/i,
                 use: ['html-loader']
             }
-        ],
-    },
+        ]
+    }
 }
 
 module.exports = () => {
     if (isProduction) {
-        config.mode = 'production'
+        config.mode = 'production',
+        config.output.clean = true
     } else {
         config.mode = 'development'
     }
